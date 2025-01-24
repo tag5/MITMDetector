40f6bf9111b3d5c6c13d29a09b98a8a8062e53c7
Julien <julien.crypt@gmail.com>
2025-01-24 18:27:03 +0100

    Notifications when network is available or lost

--- app/src/main/java/app/security/mitmdetector/BootLoader.kt
+++ app/src/main/java/app/security/mitmdetector/BootLoader.kt
@@ -5,15 +5,38 @@ import javax.inject.Inject
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.Network
 import app.security.mitmdetector.services.NotificationsService
+import javax.inject.Singleton
+
+@Singleton
+class NetworkCallback @Inject constructor(private val notificationsService: NotificationsService): ConnectivityManager.NetworkCallback() {
+    override fun onAvailable(network: Network) {
+        notificationsService.sendNotification("[tmp] network available")
+    }
+
+    override fun onLost(network: Network) {
+        notificationsService.sendNotification("[tmp] network lost")
+    }
+}
 
 @AndroidEntryPoint
 class BootLoader : BroadcastReceiver() {
+    companion object {
+        var isCallbackRegistered = false
+    }
+
     @Inject
-    lateinit var notificationsService: NotificationsService
+    lateinit var networkCallback: NetworkCallback
 
     @Override
     override fun onReceive(context: Context, intent: Intent) {
-        notificationsService.sendNotification("[tmp] BootLoader.onReceive test !")
+        if (!isCallbackRegistered) {
+            isCallbackRegistered = true
+
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            connectivityManager.registerDefaultNetworkCallback(networkCallback)
+        }
     }
 }
